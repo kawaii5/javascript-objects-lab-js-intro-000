af22e838e590f470af47ad538870a476e8173836
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-23 00:12:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
@@ -18,4 +19,5 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
+  return object;
 }
